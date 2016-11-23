3eca5a95b2298f2f721f600e552f2ffe8b54d203
Daniel Adigun <daniel.adigun@digitalforte.ng>
2016-11-23 15:27:28 +0100

    Implemented initial store and sorting by item group

--- wwwroot/js/landing.js
+++ wwwroot/js/landing.js
@@ -26,4 +26,27 @@ $(function () {
     }).mouseleave(function () {
         $('.image-overlay').hide();
     })
+
+    //send message 
+    $('form[name=send-message]').validate({
+
+        rules: {
+            email: { required: true },
+            phone: { required: true, digits:true },
+            message: { required: true },
+        },
+
+        messages: {
+            phone: {
+                digits: 'Only digits are allowed here',
+                required: 'we need your phone number pls',
+            }
+        },
+
+        submitHandler: function (form) {
+            //alert('yey!');
+            //TODO: Send Message Via Infobip SMS Gateway
+            $('.drop-message').html('<p class=\"alert alert-success\"> Successfully sent message </p>')
+        }
+    })
 });
