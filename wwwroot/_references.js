e2cbf01252d0bd6dca0a83b0c329abf708645a9f
Daniel Adigun <daniel.adigun@digitalforte.ng>
2017-01-21 18:12:47 +0100

    fixed #12

--- wwwroot/_references.js
+++ wwwroot/_references.js
@@ -10,6 +10,7 @@
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/dropzone/dist/min/dropzone.min.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
+/// <reference path="lib/jquery-mask-plugin/dist/jquery.mask.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
 /// <reference path="lib/sweetalert/dist/sweetalert.min.js" />
